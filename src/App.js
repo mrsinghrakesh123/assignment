d13bfb85daa8483be35bf257e501c0930543210e
RAKESH SINGH <mrsinghrakesh@outlook.com>
2021-05-08 07:05:30 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Link, Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import ListPage from './ListPage';
+import FormPage from './FormPage';
+import Detail from './Detail';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Router>
+      <nav className="navbar navbar-default navbar-static-top">
+    <ul className="nav nav-pills">
+      <li><Link to="/listpage" activeClassName="active">USERLIST</Link></li>
+      <li><Link to="/formpage" activeClassName="active">USER PROFILE</Link></li>
+    </ul>
+  </nav>
+  <br />
+<Switch>
+<Route path="/listpage">
+  <ListPage />
+</Route>
+<Route path="/formpage">
+  <FormPage />
+</Route>
+<Route path="/detail/:id">
+  <Detail />
+</Route>
+</Switch>
+</Router>
     </div>
   );
 }
